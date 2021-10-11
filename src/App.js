779c35c0ef94c1edffb67b6ff65d8c138dd8c220
aaryantechis <aaryan@techis.io>
2021-10-11 09:40:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+const axios = require("axios");
 
 function App() {
+  const api = axios.create({
+    baseURL: "https://www.kayak.com/h/mobileapis/directory/airlines/homework",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  // Make a request for a user with a given ID
+  api
+    .get("/")
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world</h1>
     </div>
   );
 }
